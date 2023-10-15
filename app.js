bf28349a854fbc466f5f2fea8df577c6331513d0
shamrasal <shameshrasal77@gmail.com>
2023-10-15 13:13:52 +0530

    express added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText)
+const express = require('express');
 
-const server = http.createServer(routes.requestHandler)
+const app = express()
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('thi8s always runs')
+    next()
+})
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<body>add product</body>')
+})
+
+app.use((req, res, next) => {
+    console.log('in another middleware')
+    res.send('<body>Hello from express</body>')
+})
+
+app.listen(3000);
 
